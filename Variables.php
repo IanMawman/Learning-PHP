3ed1b604ec791e39847caa082b25cbca473e0290
Ian M <31282064+IanMawman@users.noreply.github.com>
2023-12-05 19:30:09 +0000

    Add files via upload

---
+++ Variables.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Outputting Variables</title>
+</head>
+<body>
+     <?php
+     
+     $variable1 = "Hello";
+     $variable2 = "World";
+?>
+    <h1><?php echo $variable1 . "" . $variable2 ; ?></h1>
+</body>
+</html>
+

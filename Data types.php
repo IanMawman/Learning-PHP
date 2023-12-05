d8052c113690720335f4b3e3f94ad29f920e0e8b
Ian M <31282064+IanMawman@users.noreply.github.com>
2023-12-05 19:39:57 +0000

    Add files via upload

---
+++ Data types.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data types</title>
+</head>
+<body>
+     <?php
+    $String = "string";
+    echo "The value is a $String.<br>";
+    
+    $Double = 3.14;
+    echo "The value is a double or 'floating point number'.<br>";
+    
+    $boolean = true;
+    echo "The value is a boolean meaning it is either true or false.<br>";
+    
+    $integer = 42;
+    echo "The value is an integer, this is a whole number.<br>";
+    
+    $myVariable = null;
+    echo "The value is a NULL.<br>";
+?>
+    
+</body>
+</html>
+
